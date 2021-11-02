95754721d5601a2f34200bd75b4309eee63623db
GianaSister <tialeht@gmail.com>
2021-11-02 22:44:11 +0200

    Not pretty, but works

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonPressed = () => {
+    setData([...data, { key: text }]);
+    setText('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      
+      <View style={styles.button}>
+        <Button onPress={buttonPressed} title="ADD" />
+        <Button title="CLEAR" onPress={() => {
+          setText('')
+          setData([])}}>
+        </Button>
+      </View>
+     
+      <Text style={styles.header}>Shopping List</Text>
+      
+      <FlatList style={styles.list}
+        data={data}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +42,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
+  },
+  button: {
+    color: 'green',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    padding: 1,
+    margin: 1,
+  },
+  header: {
+    alignItems: 'center',
     justifyContent: 'center',
+    color: '#32CD32',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
